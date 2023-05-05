40cd775b5ca970e6ef2f0d8dc2ec6415b6977b71
behindtimes <behindtimes@gmail.com>
2023-05-05 08:58:01 -0400

    Added argument passing

--- U5AppleTiles/Program.cs
+++ U5AppleTiles/Program.cs
@@ -11,9 +11,16 @@ namespace U5AppleTiles
         static void Main(string[] args)
         {
             AppleExtractor ae = new AppleExtractor();
-            string strFile = "F:\\source\\U5Lzw\\U5AppleTiles\\SHAPES.BAK";
-            string strPng = "F:\\source\\U5Lzw\\U5AppleTiles\\SHAPES.PNG";
-            ae.Extract(strFile, strPng);
+            //string strFile = "F:\\source\\U5Lzw\\U5AppleTiles\\SHAPES.BAK";
+            //string strPng = "F:\\source\\U5Lzw\\U5AppleTiles\\SHAPES.PNG";
+            if(args.Length >= 2)
+            {
+                ae.Extract(args[0], args[1]);
+            }
+            else
+            {
+                Console.WriteLine("Args: <input SHAPES> <output png>");
+            }
         }
     }
 }
