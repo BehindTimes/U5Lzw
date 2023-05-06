ad8a54ad39ddcf857a94ef55fb7490a2832a9497
behindtimes <behindtimes@gmail.com>
2023-05-06 00:12:46 -0400

    Added program to edit the Apple II SHAPES file

---
+++ U5Apple2TileEditor/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace U5Apple2TileEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTileFile = new System.Windows.Forms.Label();
+            this.tbTileFile = new System.Windows.Forms.TextBox();
+            this.btnTFBrowse = new System.Windows.Forms.Button();
+            this.lbTileSelect = new System.Windows.Forms.ListBox();
+            this.gbTileByteState = new System.Windows.Forms.GroupBox();
+            this.pbTile = new System.Windows.Forms.PictureBox();
+            this.gbAltPalette = new System.Windows.Forms.GroupBox();
+            this.gbImage = new System.Windows.Forms.GroupBox();
+            this.btnSaveTile = new System.Windows.Forms.Button();
+            this.btnSaveShapes = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTile)).BeginInit();
+            this.gbImage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTileFile
+            // 
+            this.lblTileFile.AutoSize = true;
+            this.lblTileFile.Location = new System.Drawing.Point(12, 13);
+            this.lblTileFile.Name = "lblTileFile";
+            this.lblTileFile.Size = new System.Drawing.Size(72, 13);
+            this.lblTileFile.TabIndex = 0;
+            this.lblTileFile.Text = "SHAPES File:";
+            // 
+            // tbTileFile
+            // 
+            this.tbTileFile.Location = new System.Drawing.Point(87, 10);
+            this.tbTileFile.Name = "tbTileFile";
+            this.tbTileFile.ReadOnly = true;
+            this.tbTileFile.Size = new System.Drawing.Size(341, 20);
+            this.tbTileFile.TabIndex = 1;
+            // 
+            // btnTFBrowse
+            // 
+            this.btnTFBrowse.Location = new System.Drawing.Point(434, 8);
+            this.btnTFBrowse.Name = "btnTFBrowse";
+            this.btnTFBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnTFBrowse.TabIndex = 2;
+            this.btnTFBrowse.Text = "Browse...";
+            this.btnTFBrowse.UseVisualStyleBackColor = true;
+            this.btnTFBrowse.Click += new System.EventHandler(this.btnTFBrowse_Click);
+            // 
+            // lbTileSelect
+            // 
+            this.lbTileSelect.FormattingEnabled = true;
+            this.lbTileSelect.Location = new System.Drawing.Point(15, 46);
+            this.lbTileSelect.Name = "lbTileSelect";
+            this.lbTileSelect.Size = new System.Drawing.Size(174, 342);
+            this.lbTileSelect.TabIndex = 3;
+            this.lbTileSelect.SelectedIndexChanged += new System.EventHandler(this.lbTileSelect_SelectedIndexChanged);
+            // 
+            // gbTileByteState
+            // 
+            this.gbTileByteState.Location = new System.Drawing.Point(284, 46);
+            this.gbTileByteState.Name = "gbTileByteState";
+            this.gbTileByteState.Size = new System.Drawing.Size(268, 312);
+            this.gbTileByteState.TabIndex = 6;
+            this.gbTileByteState.TabStop = false;
+            this.gbTileByteState.Text = "Tile Byte State";
+            // 
+            // pbTile
+            // 
+            this.pbTile.Location = new System.Drawing.Point(21, 34);
+            this.pbTile.Name = "pbTile";
+            this.pbTile.Size = new System.Drawing.Size(224, 256);
+            this.pbTile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTile.TabIndex = 7;
+            this.pbTile.TabStop = false;
+            this.pbTile.Paint += new System.Windows.Forms.PaintEventHandler(this.pbTile_Paint);
+            // 
+            // gbAltPalette
+            // 
+            this.gbAltPalette.Location = new System.Drawing.Point(208, 46);
+            this.gbAltPalette.Name = "gbAltPalette";
+            this.gbAltPalette.Size = new System.Drawing.Size(70, 312);
+            this.gbAltPalette.TabIndex = 7;
+            this.gbAltPalette.TabStop = false;
+            this.gbAltPalette.Text = "Alt Palette";
+            // 
+            // gbImage
+            // 
+            this.gbImage.Controls.Add(this.pbTile);
+            this.gbImage.Location = new System.Drawing.Point(558, 46);
+            this.gbImage.Name = "gbImage";
+            this.gbImage.Size = new System.Drawing.Size(268, 312);
+            this.gbImage.TabIndex = 7;
+            this.gbImage.TabStop = false;
+            this.gbImage.Text = "Image";
+            // 
+            // btnSaveTile
+            // 
+            this.btnSaveTile.Location = new System.Drawing.Point(751, 368);
+            this.btnSaveTile.Name = "btnSaveTile";
+            this.btnSaveTile.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveTile.TabIndex = 8;
+            this.btnSaveTile.Text = "Save Tile";
+            this.btnSaveTile.UseVisualStyleBackColor = true;
+            this.btnSaveTile.Click += new System.EventHandler(this.btnSaveTile_Click);
+            // 
+            // btnSaveShapes
+            // 
+            this.btnSaveShapes.Location = new System.Drawing.Point(751, 10);
+            this.btnSaveShapes.Name = "btnSaveShapes";
+            this.btnSaveShapes.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveShapes.TabIndex = 9;
+            this.btnSaveShapes.Text = "Save File";
+            this.btnSaveShapes.UseVisualStyleBackColor = true;
+            this.btnSaveShapes.Click += new System.EventHandler(this.btnSaveShapes_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 403);
+            this.Controls.Add(this.btnSaveShapes);
+            this.Controls.Add(this.btnSaveTile);
+            this.Controls.Add(this.gbImage);
+            this.Controls.Add(this.gbAltPalette);
+            this.Controls.Add(this.gbTileByteState);
+            this.Controls.Add(this.lbTileSelect);
+            this.Controls.Add(this.btnTFBrowse);
+            this.Controls.Add(this.tbTileFile);
+            this.Controls.Add(this.lblTileFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTile)).EndInit();
+            this.gbImage.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTileFile;
+        private System.Windows.Forms.TextBox tbTileFile;
+        private System.Windows.Forms.Button btnTFBrowse;
+        private System.Windows.Forms.ListBox lbTileSelect;
+        private System.Windows.Forms.GroupBox gbTileByteState;
+        private System.Windows.Forms.PictureBox pbTile;
+        private System.Windows.Forms.GroupBox gbAltPalette;
+        private System.Windows.Forms.GroupBox gbImage;
+        private System.Windows.Forms.Button btnSaveTile;
+        private System.Windows.Forms.Button btnSaveShapes;
+    }
+}
+
---
+++ U5Apple2TileEditor/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U5Apple2TileEditor
+{
+    public partial class Form1 : Form
+    {
+        byte[] file_bytes = null;
+        CheckBox[,] cbBSArray;
+        CheckBox[,] cbArray;
+
+        Color cGreen;
+        Color cOrange;
+        Color cPurple;
+        Color cBlue;
+        Bitmap origBMP;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cGreen = Color.FromArgb(67, 195, 0); // green
+            cOrange = Color.FromArgb(234, 93, 21); // orange
+            cPurple = Color.FromArgb(182, 61, 255); // purple
+            cBlue = Color.FromArgb(16, 164, 227); // blue
+            origBMP = new Bitmap(14, 16);
+        }
+
+        private void btnTFBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            DialogResult result = ofd.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                tbTileFile.Text = ofd.FileName;
+
+                try
+                {
+                    file_bytes = File.ReadAllBytes(tbTileFile.Text);
+                    if (file_bytes.Length == 0x4000)
+                    {
+                        lbTileSelect.Items.Clear();
+                        for (int index = 0; index < 512; ++index)
+                        {
+                            string strItem = "Tile " + index;
+                            lbTileSelect.Items.Add(strItem);
+                        }
+                        lbTileSelect.SelectedIndex = 0;
+                    }
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void lbTileSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(null == file_bytes)
+            {
+                return;
+            }
+
+            for (int xPos = 0; xPos < 2; xPos++)
+            {
+                for (int yPos = 0; yPos < 16; yPos++)
+                {
+                    cbBSArray[xPos, yPos].Checked = false;
+                }
+            }
+
+            for (int xPos = 0; xPos < 14; xPos++)
+            {
+                for (int yPos = 0; yPos < 16; yPos++)
+                {
+                    cbArray[xPos, yPos].Checked = false;
+                }
+            }
+
+            int curId = lbTileSelect.SelectedIndex;
+            int offset = 0;
+            if(curId >= 256)
+            {
+                offset = 0x2000;
+                curId %= 256;
+            }
+            
+            for(int hIndex = 0; hIndex < 2; hIndex++)
+            {
+                int tempOffset = curId + offset + (hIndex * 0x1000);
+                int tempXOffset = hIndex * 7;
+
+                for (int vIndex = 0; vIndex < 16; vIndex++)
+                {
+                    byte curByte = file_bytes[tempOffset];
+                    int colorType = (curByte >> 7) & 0x01;
+
+                    cbBSArray[hIndex, vIndex].Checked = colorType != 0;
+
+                    for (int index = 0; index < 7; ++index)
+                    {
+                        int curBit = (curByte >> index) & 0x01;
+                        cbArray[index + tempXOffset, vIndex].Checked = (curBit == 1);
+                    }
+
+                    tempOffset += 0x100;
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbBSArray = new CheckBox[2, 16];
+
+            for (int xPos = 0; xPos < 2; xPos++)
+            {
+                for (int yPos = 0; yPos < 16; yPos++)
+                {
+                    cbBSArray[xPos, yPos] = new CheckBox();
+                    cbBSArray[xPos, yPos].Parent = gbAltPalette;
+                    cbBSArray[xPos, yPos].Appearance = Appearance.Button;
+                    cbBSArray[xPos, yPos].AutoSize = false;
+                    cbBSArray[xPos, yPos].Size = new Size(16, 16);
+                    cbBSArray[xPos, yPos].Text = "";
+                    cbBSArray[xPos, yPos].Location = new Point(10 + (18 * xPos), 18 + (18 * yPos));
+                    cbBSArray[xPos, yPos].CheckedChanged += Form1_CheckedChanged;
+                    cbBSArray[xPos, yPos].Tag = new Point(xPos, yPos);
+                }
+            }
+
+            cbArray = new CheckBox[14, 16];
+            for (int xPos = 0; xPos < 14; xPos++)
+            {
+                for (int yPos = 0; yPos < 16; yPos++)
+                {
+                    cbArray[xPos, yPos] = new CheckBox();
+                    cbArray[xPos, yPos].Parent = gbTileByteState;
+                    cbArray[xPos, yPos].Appearance = Appearance.Button;
+                    cbArray[xPos, yPos].AutoSize = false;
+                    cbArray[xPos, yPos].Size = new Size(16, 16);
+                    cbArray[xPos, yPos].Text = "";
+                    cbArray[xPos, yPos].Location = new Point(10 + (18 * xPos), 18 + (18 * yPos));
+                    cbArray[xPos, yPos].CheckedChanged += Form1_CheckedChanged;
+                    cbArray[xPos, yPos].Tag = new Point(xPos, yPos);
+                }
+            }
+            
+        }
+
+        private void setColor(int curCol, int curRow)
+        {
+            // No need to check out of bounds
+            if(curCol < 0 || curCol > 13)
+            {
+                return;
+            }
+            bool bIsChecked = cbArray[curCol, curRow].Checked;
+            int paletteVal = 0;
+            bool nextVal = false;
+            bool prevVal = false;
+
+            if (curCol + 1 < 14)
+            {
+                nextVal = cbArray[curCol + 1, curRow].Checked;
+            }
+            if (curCol > 0)
+            {
+                prevVal = cbArray[curCol - 1, curRow].Checked;
+            }
+
+            if (curCol > 6)
+            {
+                paletteVal = 1;
+            }
+            bool bPaletteChecked = cbBSArray[paletteVal, curRow].Checked;
+            if (bIsChecked)
+            {
+                if (!nextVal && !prevVal)
+                {
+                    origBMP.SetPixel(curCol, curRow, Color.White);
+                    if (curCol % 2 == 0)
+                    {
+                        if (bPaletteChecked)
+                        {
+                            origBMP.SetPixel(curCol, curRow, cOrange);
+                        }
+                        else
+                        {
+                            origBMP.SetPixel(curCol, curRow, cGreen);
+                        }
+                    }
+                    else
+                    {
+                        if (bPaletteChecked)
+                        {
+                            origBMP.SetPixel(curCol, curRow, cBlue);
+                        }
+                        else
+                        {
+                            origBMP.SetPixel(curCol, curRow, cPurple);
+                        }
+                    }
+                }
+                else
+                {
+                    origBMP.SetPixel(curCol, curRow, Color.White);
+                }
+            }
+            else
+            {
+                if (nextVal && prevVal)
+                {
+                    // fringing occured
+                    if (curCol % 2 == 0)
+                    {
+                        if (bPaletteChecked)
+                        {
+                            origBMP.SetPixel(curCol, curRow, cBlue);
+                        }
+                        else
+                        {
+                            origBMP.SetPixel(curCol, curRow, cPurple);
+                        }
+                       
+                    }
+                    else
+                    {
+                        if (bPaletteChecked)
+                        {
+                            origBMP.SetPixel(curCol, curRow, cOrange);
+                        }
+                        else
+                        {
+                            origBMP.SetPixel(curCol, curRow, cGreen);
+                        }
+                    }
+                }
+                else
+                {
+                    origBMP.SetPixel(curCol, curRow, Color.Black);
+                }
+            }
+        }
+
+        private void Form1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(((CheckBox)sender).Tag != null)
+            {
+                if (((CheckBox)sender).Tag is Point)
+                {
+                    int curRow = (((Point)((CheckBox)sender).Tag)).Y;
+                    for(int indexX = 0; indexX < 14; indexX++)
+                    {
+                        setColor(indexX, curRow);
+                    }
+
+                    pbTile.Refresh();
+                }
+            }
+        }
+
+        private void pbTile_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+            e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
+            e.Graphics.DrawImage(
+                origBMP,
+            new Rectangle(0, 0, pbTile.Width, pbTile.Height),
+            // destination rectangle 
+            0,
+            0,           // upper-left corner of source rectangle
+            origBMP.Width,       // width of source rectangle
+            origBMP.Height,      // height of source rectangle
+            GraphicsUnit.Pixel);
+        }
+
+        private void btnSaveTile_Click(object sender, EventArgs e)
+        {
+            if(file_bytes == null)
+            {
+                return;
+            }
+
+            int curId = lbTileSelect.SelectedIndex;
+            int offset = 0;
+            if (curId >= 256)
+            {
+                offset = 0x2000;
+                curId %= 256;
+            }
+
+            for (int hIndex = 0; hIndex < 2; hIndex++)
+            {
+                int tempOffset = curId + offset + (hIndex * 0x1000);
+                int tempXOffset = hIndex * 7;
+
+               
+
+                for (int vIndex = 0; vIndex < 16; vIndex++)
+                {
+                    byte curByte = 0;
+                    int colorType = (curByte >> 7) & 0x01;
+
+                    if(cbBSArray[hIndex, vIndex].Checked)
+                    {
+                        curByte = 1;
+                    }
+
+                    for (int index = 6; index >= 0; --index)
+                    {
+                        curByte <<= 1;
+                        if(cbArray[index + tempXOffset, vIndex].Checked)
+                        {
+                            curByte |= 1;
+                        }
+                    }
+                    file_bytes[tempOffset] = curByte;
+                    tempOffset += 0x100;
+                }
+            }
+        }
+
+        private void btnSaveShapes_Click(object sender, EventArgs e)
+        {
+            if (file_bytes == null)
+            {
+                return;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            DialogResult result = sfd.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                try
+                {
+                    using (BinaryWriter binWriter =
+                        new BinaryWriter(File.Open(sfd.FileName, FileMode.Create)))
+                    {
+                        binWriter.Write(file_bytes);
+                    }
+                }
+                catch (IOException ioexp)
+                {
+                    Console.WriteLine("Error: {0}", ioexp.Message);
+                }
+            }
+        }
+    }
+}
